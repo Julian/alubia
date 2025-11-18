e1f0dc880f48b03de135759ad8f937fc8745a15f
Julian Berman <Julian@GrayVines.com>
2025-11-18 12:57:21 +0100

    Add a simple csv helper for ingestion.

---
+++ alubia/ingest.py
@@ -0,0 +1,27 @@
+"""
+Ingestion helpers for reading in files.
+"""
+
+from __future__ import annotations
+
+from csv import DictReader
+from datetime import date
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+def from_csv(
+    path: Path,
+    date_column: str = "date",
+) -> tuple[date, dict[str, Any]]:
+    """
+    Partially parse a given csv path.
+    """
+    with path.open(newline="") as contents:
+        reader = DictReader(contents)
+        for row in reader:
+            date_str = row.pop(date_column)
+            row_date = date.strptime(date_str, "%m/%d/%Y")
+            yield row_date, row
