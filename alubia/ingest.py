0b613404f3981832a5ea357cbd8746146182bc42
Julian Berman <Julian@GrayVines.com>
2025-11-21 08:28:41 -0500

    Pull dates and payees via arbitraryc callables.
    Supports e.g. columns with extra noise in them.

--- alubia/ingest.py
+++ alubia/ingest.py
@@ -5,26 +5,24 @@ Ingestion helpers for reading in files.
 from __future__ import annotations
 
 from csv import DictReader
-from datetime import date
 from typing import TYPE_CHECKING, Any
+import datetime
 
 if TYPE_CHECKING:
-    from collections.abc import Iterable
+    from collections.abc import Callable, Iterable
     from pathlib import Path
 
 
 def from_csv(
     path: Path,
-    date_from: str = "Date",
-    payee_from: str = "Description",
-) -> Iterable[tuple[date, str, dict[str, Any]]]:
+    date: Callable[[dict[str, Any]], str] = lambda row: row["Date"],
+    payee: Callable[[dict[str, Any]], str] = lambda row: row["Description"],
+) -> Iterable[tuple[datetime.date, str, dict[str, Any]]]:
     """
     Partially parse a given csv path.
     """
     with path.open(newline="") as contents:
         reader = DictReader(contents)
         for row in reader:
-            date_str = row.pop(date_from)
-            row_date = date.strptime(date_str, "%m/%d/%Y")
-            payee = row.pop(payee_from).strip()
-            yield row_date, payee, row
+            row_date = datetime.date.strptime(date(row), "%m/%d/%Y")
+            yield row_date, payee(row).strip(), row
